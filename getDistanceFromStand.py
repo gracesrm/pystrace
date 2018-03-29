893ebb429cc3393b693af6ca93a09e20fa631a4d
gracesrm <gracesrm@ufl.edu>
2018-03-29 13:19:06 -0400

    zhengli

--- getDistanceFromStand.py
+++ getDistanceFromStand.py
@@ -13,19 +13,26 @@ import numpy as np
 import pickle
 
 def Start(stand_dir, unpred_dir, c):
-	subdir = unpred_dir.split('/')[-2]
-	print subdir
 	for rt, drs, strfs in os.walk(unpred_dir):
 		for strf_unp in strfs:
+
+			unpred_src = os.path.join(rt, strf_unp)
+			subdir = unpred_src.split('/')[-2] + '/'
+			# print subdir
+
 			if strf_unp[-4:] == ".str":
 				print strf_unp
 				strf_std = stand_dir + strf_unp[:-6] + ".str"
-				print strf_std
-				with open(strf_std,'r') as str_fd, open(os.path.join(rt,strf_unp), 'r') as str_unp_fd:
+				# print strf_std
+				with open(strf_std,'r') as str_fd, open(os.path.join(rt,unpred_src), 'r') as str_unp_fd:
 					ref = str_fd.readlines()[int(c)]
 					comp = str_unp_fd.readline()
-					dis = editdistance.eval(ref, comp)
-					dest_file = "/home/ruimin/pystrace/disdir/" + subdir + "/" + strf_unp[:-4] + ".dis"	
+					try:
+						dis = editdistance.eval(ref, comp)
+					except:
+						pass
+					dest_file = "/home/ruimin/pystrace/disdir/" + subdir + strf_unp[:-6] + ".dis"	
+					# print dest_file
 					with open(dest_file,'ab') as f:
 						f.write(str(dis) + '\n')
 
